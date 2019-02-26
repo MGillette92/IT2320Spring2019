0f9c834b194a5bc5a86727d231306f2c4d44a51e
MGillette92 <36048864+MGillette92@users.noreply.github.com>
2019-02-25 23:31:31 -0500

    fixed bugs
    fixed bugs in the code to lab specifications

--- Lab4/sample.js
+++ Lab4/sample.js
@@ -1,7 +1,6 @@
-function displayFruits()
-{
-    document.getElementById("display-panel").innerHTML="";
-    
+function displayFruits() {
+    document.getElementById("display-panel").innerHTML = "";
+
     //Find the bug(s) in the code
     //We've classified fruits into 3 categories
     //We want all thh fruits to display in their right category
@@ -9,23 +8,21 @@ function displayFruits()
     //subtropical:avocado,guava
     //not tropical:apple, grapes, kiwi, watermelon
 
-    var fruits = ["apple","orange","banana","grapes","guava","coconut","grapefruit","kiwi","watermelon","pineapple","avocado"];
-    var displayText="";
+    var fruits = ["apple", "orange", "banana", "grapes", "guava", "coconut", "grapefruit", "kiwi", "watermelon", "pineapple", "avocado"];
+    var displayText = "";
 
-    for (i=0;i<5;i++)
-    {
-        displayText += "<p>"+ fruits[i];    
-        displayText += " is "+ IsTropical(fruits[i]);
-        displayText+="</p>";        
-    }    
-    document.getElementById("display-panel").innerHTML=displayText;
+    for (i = 0; i < fruits.length; i++) {
+        displayText += "<p>" + fruits[i];
+        displayText += " is " + IsTropical(fruits[i]);
+        displayText += "</p>";
+    }
+    document.getElementById("display-panel").innerHTML = displayText;
 }
 
-function IsTropical(fruit)
-{
+function IsTropical(fruit) {
     var tropicalMsg;
 
-     switch (fruit){
+    switch (fruit) {
         case "apple":
             tropicalMsg = "not tropical";
             break;
@@ -34,114 +31,118 @@ function IsTropical(fruit)
             break;
         case "coconut":
             tropicalMsg = "tropical";
-        case "grapes":          
+            break;
+        case "grapes":
             tropicalMsg = "not tropical";
             break;
         case "orange":
-            tropicalMsg = "tropical";           
+            tropicalMsg = "tropical";
+            break;
         case "papaya":
-            tropicalMsg = "tropical";           
+            tropicalMsg = "tropical";
             break;
         case "peach":
-            tropicalMsg = "tropical";                          
-        case "guava":      
-            tropicalMsg = "sub tropical";           
-            break;   
-        case "pineapple":      
-            tropicalMsg = "tropical";           
+            tropicalMsg = "tropical";
+            break;
+        case "guava":
+            tropicalMsg = "sub tropical";
+            break;
+        case "pineapple":
+            tropicalMsg = "tropical";
             break;
         case "kiwi":
-            tropicalMsg = "tropical";           
+            tropicalMsg = "tropical";
+            break;
         case "avocado":
             tropicalMsg = "sub tropical";
+            break;
         case "grapefruit":
-            tropicalMsg = "tropical";           
+            tropicalMsg = "tropical";
             break;
         default:
-            tropicalMsg = "unable to determine fruit type";            
+            tropicalMsg = "unable to determine fruit type";
     }
-   
+
     return tropicalMsg;
 }
-function AddNumbers()
-{
-    document.getElementById("display-panel").innerHTML="";
+function AddNumbers() {
+    document.getElementById("display-panel").innerHTML = "";
 
-    var numbers = [2,4,6,8,10,12,14];
+    var numbers = [2, 4, 6, 8, 10, 12, 14];
+    var sum = 0;
 
-    for (i=1;i<=numbers.length;i++)
-    {
-        var sum=0;    
-        sum+=numbers[i];
+    for (i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
     }
-    
+
     //This is where we display the sum
-    document.getElementById("display-panel").innerHTML="Sum of numbers "+numbers+" is equal to "+sum;
-    
+    document.getElementById("display-panel").innerHTML = "Sum of numbers " + numbers + " is equal to " + sum;
+
 }
 
-function EvenOrOddCalc()
-{    
+function EvenOrOddCalc() {
     var evenOddFlag = EvenOrOdd;
     console.log(evenOddFlag);
 }
 
 
-function EvenOrOdd()
-{    
+function EvenOrOdd() {
     //In this, identify the even and odd numbers
-    document.getElementById("display-panel").innerHTML="";
-    var evenOdd = [13,4,2,67,94,35,654];
-    var even=odd="";
-    var i=j=0;
-    var flag=0;
+    document.getElementById("display-panel").innerHTML = "";
+    var evenOdd = [13, 4, 2, 67, 94, 35, 654];
+    var even = odd = "";
+    var i = j = 0;
+    var flag = 0;
 
-   for(i=0;i<evenOdd.length;i++)    
-   {
-        flag = evenOdd[i]%2;
-        if (flag = 0)
-            even+=" "+evenOdd[i];
+    for (i = 0; i < evenOdd.length; i++) {
+        flag = evenOdd[i] % 2;
+        if (flag == 0)
+            even += " " + evenOdd[i];
         else
-            odd+=" "+evenOdd[i];
-        
+            odd += " " + evenOdd[i];
+
     };
-    document.getElementById("display-panel").innerHTML="<p>Given numbers: "+evenOdd+"</p>";
-    document.getElementById("display-panel").innerHTML+="<p>Even:  "+even+" Odd: "+odd+"</p>";
-    
+    document.getElementById("display-panel").innerHTML = "<p>Given numbers: " + evenOdd + "</p>";
+    document.getElementById("display-panel").innerHTML += "<p>Even:  " + even + " Odd: " + odd + "</p>";
+
     return true;
 }
 
 
-function GenerateNumbers()
-{
+function GenerateNumbers() {
     //There are 9 prime numbers and 10 odd numbers.Fix the bugs so the right count displays
 
-    var num = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+    var num = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 23];
     var factorFound;
-    var prime=0;
-    for (i=0;i<num.length;i++)
-    {  
-        
+    var prime = 0;
+    var odd = 0;
+
+    for (i = 0; i < num.length; i++) {
+
         if (num[i] == 2)
             prime++;
-        else
-        {
-            for (j=2;j<num[i];j++) //Check from 2 to num-1 and see if the number has factors other than 1 and itself
+        else {
+            for (j = 2; j < num[i]; j++) //Check from 2 to num-1 and see if the number has factors other than 1 and itself
             {
-                if ((num[i] % j)==0)                 
-                    factorFound++;                    
+                {
+                    if ((num[i] % j) == 0)
+                        factorFound++;
+
+                }
+                
             }
-        
+
             if (!factorFound) //No factors
                 prime++;
         }
 
-        var odd=0;
-        if (num[i]%2 != 0)
+        factorFound = null;
+
+        if (num[i] % 2 != 0)
             odd++;
 
-   }
-    document.getElementById("display-panel").innerHTML="Given array "+num;
-    document.getElementById("display-panel").innerHTML+=" Prime count = "+prime;
-    document.getElementById("display-panel").innerHTML+=" Odd count "+ odd;
+    }
+    document.getElementById("display-panel").innerHTML = "Given array " + num;
+    document.getElementById("display-panel").innerHTML += " Prime count = " + prime;
+    document.getElementById("display-panel").innerHTML += " Odd count " + odd;
 }
